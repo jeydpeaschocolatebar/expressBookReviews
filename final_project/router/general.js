4c14b893c9c6d3bd328c41420efd0a43c0219b1f
Jade Francis Palco <40660814+jeydpeaschocolatebar@users.noreply.github.com>
2025-06-16 09:47:57 +0800

    work in progress

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,22 +33,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
-  const [result] = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase());
-  if (result) {
+  const result = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase());
+  if (result.length > 0) {
     return res.status(200).json(result);
   } else {
-    return res.status(404).json({message: "Book by this author not found"});
+    return res.status(404).json({message: "Books by this author not found"});
   }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const title = req.params.title;
-  const [result] = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase());
-  if (result) {
+  const result = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase());
+  if (result.length > 0) {
     return res.status(200).json(result);
   } else {
-    return res.status(404).json({message: "Book by this author not found"});
+    return res.status(404).json({message: "Books by this title not found"});
   }
 });
 
